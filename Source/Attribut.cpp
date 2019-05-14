c78359ff6a9cb5c940d50eff0cfb639379a5c54a
BriceVachez <50611448+BriceVachez@users.noreply.github.com>
2019-05-14 09:44:55 +0200

    Add files via upload

---
+++ Source/Attribut.cpp
@@ -0,0 +1,78 @@
+/*************************************************************************
+analyser  -  description
+-------------------
+début                : $DATE$
+copyright            : (C) $YEAR$ par $AUTHOR$
+e-mail               : $EMAIL$
+*************************************************************************/
+
+//---------- Réalisation de la classe <analyser> (fichier Attribut.cpp) ------------
+
+//---------------------------------------------------------------- INCLUDE
+
+//-------------------------------------------------------- Include système
+#include <iostream>
+using namespace std;
+
+//------------------------------------------------------ Include personnel
+#include "Attribut.h"
+
+
+//------------------------------------------------------------- Constantes
+
+//----------------------------------------------------------------- PUBLIC
+
+//----------------------------------------------------- Méthodes publiques
+// type analyser::Méthode ( liste des paramètres )
+// Algorithme :
+//
+//{
+//} //----- Fin de Méthode
+
+//-------------------------------------------- Constructeurs - destructeur
+Attribut::Attribut(const Attribut & unAttribut)
+{
+	this.attributeId = unAttribut.attributeId;
+
+	this.unit = unAttribut.unit;
+
+	this.description = unAttribut.description;
+
+	this.valeurSeuil = unAttribut.valeurSeuil;
+// Mode d'emploi (constructeur de copie) :
+//
+// Contrat :
+//
+
+Attribut::Attribut() {}
+// Mode d'emploi :
+//
+// Contrat :
+//
+
+Attribut::Attribut(std::string attributeId, std::string unit, std::string description, int valeurSeuil)
+{
+	this.attributeId = attributeId;
+
+	this.unit = unit;
+
+	this.description = description;
+
+	this.valeurSeuil = valeurSeuil;
+	}
+// Mode d'emploi :
+//
+// Contrat :
+//
+
+virtual Attribut::~Attribut() {}
+// Mode d'emploi :
+//
+// Contrat :
+//
+
+
+  //------------------------------------------------------------------ PRIVE
+
+  //----------------------------------------------------- Méthodes protégées
+
---
+++ Source/Mesure.cpp
@@ -0,0 +1,93 @@
+/*************************************************************************
+analyser  -  description
+-------------------
+début                : $DATE$
+copyright            : (C) $YEAR$ par $AUTHOR$
+e-mail               : $EMAIL$
+*************************************************************************/
+
+//---------- Réalisation de la classe <analyser> (fichier Meusre.cpp) ------------
+
+//---------------------------------------------------------------- INCLUDE
+
+//-------------------------------------------------------- Include système
+#include <iostream>
+using namespace std;
+
+//------------------------------------------------------ Include personnel
+#include "Mesure.h"
+
+
+//------------------------------------------------------------- Constantes
+
+//----------------------------------------------------------------- PUBLIC
+
+//----------------------------------------------------- Méthodes publiques
+// type analyser::Méthode ( liste des paramètres )
+// Algorithme :
+//
+//{
+//} //----- Fin de Méthode
+
+//-------------------------------------------- Constructeurs - destructeur
+Mesure::Mesure(const Mesure & uneMesure)
+// Algorithme :
+//
+{
+#ifdef MAP
+	cout << "Appel au constructeur de copie de <Mesure>" << endl;
+#endif
+
+	this.timestamp = uneMesure.timestamp;
+
+	this.sensorId = uneMesure.sensorId;
+
+	this.attributeId = uneMesure.attributeId;
+
+	this.value = uneMesure.value;
+
+} //----- Fin de analyser (constructeur de copie)
+
+Mesure::Mesure(Time timestamp, std::string sensorId, std::string attributeId, int value)
+{
+#ifdef MAP
+	cout << "Appel au constructeur de <Mesure>" << endl;
+#endif
+
+	this.timestamp = uneMesure.timestamp;
+
+	this.sensorId = uneMesure.sensorId;
+
+	this.attributeId = uneMesure.attributeId;
+
+	this.value = uneMesure.value;
+}
+
+Mesure::Mesure()
+// Algorithme :
+//
+{
+#ifdef MAP
+	cout << "Appel au constructeur de <Mesure>" << endl;
+#endif
+} //----- Fin de analyser
+
+Mesure::~Mesure()
+// Algorithme :
+//
+{
+#ifdef MAP
+	cout << "Appel au destructeur de <Mesure>" << endl;
+#endif
+} //----- Fin de ~analyser
+
+Mesure::Warning alerterSurMesure()
+{
+
+}
+
+
+  //------------------------------------------------------------------ PRIVE
+
+//----------------------------------------------------- Méthodes protégées
+
