6a30caff02d327dc4f82a2aa4860898b502f1a99
solenebutruille <47445746+solenebutruille@users.noreply.github.com>
2019-05-22 15:07:43 +0200

    Add files via upload

--- En-tete/Capteur.h
+++ En-tete/Capteur.h
@@ -1,16 +1,6 @@
-/*************************************************************************
-						   Catalogue  -  description
-							 -------------------
-	début                : 30/11/2018
-	copyright            : (C) 2018 par YE Linda, YE Zihang
-	e-mail               : linda.ye@insa-lyon.fr zihang.ye@insa-lyon.fr
-*************************************************************************/
-
-//---- Interface de la classe <Catalogue> (fichier Catalogue.h)  ---------
-#if ! defined ( CAPTEUR_H )
+#ifndef CAPTEUR_H
 #define CAPTEUR_H
 
-//--------------------------------------------------- Interfaces utilisées
 #include <iostream>
 #include <string>
 using namespace std;
@@ -18,35 +8,30 @@ using namespace std;
 class Capteur
 {
 public:
-	// Constructeurs
-	Capteur();
-	Capteur(string sensorId, int longitude, int latitude, string description);
-	Capteur(const Capteur &);
+  // Constructeurs
+  Capteur();
+  Capteur(string sensorId, int longitude, int lattitude, string description);
 
-	//Accesseurs et mutateurs
-	void setSensorId(string sensorId);
-	void setLongitude(int longitude);
-	void setDescription(string description);
-	void setLattitude(int lattitude);
-	int getLongitude() const;
-	int getLatitude() const;
-	string getSensorId() const;
-	string getDescription() const;
+  //Accesseurs et mutateurs
+  void setSensorId(string sensorId);
+  void setLongitude(int longitude);
+  void setDescription(string description);
+  void setLattitude(int lattitude);
+  int getLongitude() const;
+  int getLattitude() const;
+  string getSensorId() const;
+  string getDescription() const;
 
-	// Autres méthodes
-	string caracteristiqueDeLair() const;
+  // Autres mÃ©thodes
+  string caracteristiqueDeLair() const;
 
-	void afficher() const;
+  void afficher() const;
 
-	//------------------------------------------------- Surcharge d'opérateurs
-
-	bool operator==(const Capteur & c2) const;
-
-protected:
-	string sensorId;
-	int longitude;
-	int latitude;
-	string description;
+private: 
+  string sensorId;
+  int longitude;
+  int lattitude;
+  string description;
 };
 
 #endif
--- En-tete/GestionCapteur.h
+++ En-tete/GestionCapteur.h
@@ -1,85 +1,4 @@
 /*************************************************************************
-<<<<<<< HEAD
-						   Gestion Capteur  -  description
-							 -------------------
-	début                : 14/05/2019
-	copyright            : (C) 2019 par GALL Roxane
-	e-mail               : roxane.gall@insa-lyon.fr
-*************************************************************************/
-
-//---- Interface de la classe <Catalogue> (fichier Catalogue.h)  ---------
-#if ! defined ( GESTIONCAPTEUR_H )
-#define GESTIONCAPTEUR_H
-
-//--------------------------------------------------- Interfaces utilisées
-#include <cstring>
-#include <vector>
-using namespace std;
-
-#include "Capteur.h"
-
-
-class GestionCapteur
-{
-	//----------------------------------------------------------------- PUBLIC
-
-public:
-	//----------------------------------------------------- Méthodes publiques
-	string afficherCapteur() const;
-	// Mode d'emploi :
-	//
-	// Contrat :
-	//
-
-	bool ajouterCapteur(Capteur c);
-	// Mode d'emploi :
-	//
-	// Contrat :
-	//
-
-	bool supprimerCapteur(Capteur c);
-	// Mode d'emploi :
-	//
-	// Contrat :
-	//
-
-	bool surveillerCapteur(string id) const;
-	// Mode d'emploi :
-	//
-	// Contrat :
-	//
-
-	Capteur rechercherCapteur(string id) const;
-	// Mode d'emploi :
-	//
-	// Contrat :
-	//
-
-	Capteur rechercherCapteur(int latitude, int longitude) const;
-	// Mode d'emploi :
-	//
-	// Contrat :
-	//
-
-	vector<Capteur> capteursSimilaires(string SensorId);
-	// Mode d'emploi :
-	//
-	// Contrat :
-	//
-
-	//------------------------------------------------------------------ PRIVE
-
-protected:
-	//----------------------------------------------------- Méthodes protégées
-
-	//----------------------------------------------------- Attributs protégés
-	vector<Capteur> listCapteur;
-};
-
-//-------------------------------- Autres définitions dépendantes de <GestionCapteur>
-
-#endif // GESTIONCAPTEUR_H
-=======
                            GestionMesure  -  description
                              -------------------
     dï¿½but                : ${date}
@@ -96,7 +15,7 @@ protected:
 #include <map>
 #include <vector>
 #include <time.h>
-#include "Capteur.h"
+#include "../En-tete/Capteur.h"
 #include <string>
 using namespace std;
 //------------------------------------------------------------- Constantes
@@ -183,4 +102,3 @@ public:
 //----------------------------------------- Types dï¿½pendants de <GestionMesure>
 
 #endif // GESTIONCAPTEUR_H
->>>>>>> 53a0ae5d333d37c659bbae42608dbb316434e22e
