dd4f27bb05b757465675cba94061cb4446c31337
BrunoSader <brunosader@hotmail.com>
2019-05-22 09:44:10 +0200

    Warning

--- Source/main.cpp
+++ Source/main.cpp
@@ -20,39 +20,38 @@ using namespace std;
 #include "../En-tete/Attribut.h"
 
 
-int main()
-{
+int main(){
 	map<string,map<time_t,map<string,int>>> listeMesurebyCapteur;
 	map<time_t,map<string,int>> listeMesurebyDate;
 	map<string,int> listeMesurebyAttribut;
 
 	listeMesurebyAttribut.insert( std::pair<string,int>("O3",100) );
-	listeMesurebyAttribut.insert( std::pair<string,int>("N3",100) );
+	listeMesurebyAttribut.insert( std::pair<string,int>("N3",50) );
 	time_t date;
 	listeMesurebyDate.insert( std::pair<time_t,map<string,int>>(date,listeMesurebyAttribut) );
 
 	listeMesurebyAttribut.insert( std::pair<string,int>("O3",50) );
-	listeMesurebyAttribut.insert( std::pair<string,int>("N3",50) );
+	listeMesurebyAttribut.insert( std::pair<string,int>("N3",2) );
 	time_t date2;
 	listeMesurebyDate.insert( std::pair<time_t,map<string,int>>(date2,listeMesurebyAttribut) );
 
 	listeMesurebyAttribut.insert( std::pair<string,int>("O3",50) );
-	listeMesurebyAttribut.insert( std::pair<string,int>("N3",50) );
+	listeMesurebyAttribut.insert( std::pair<string,int>("N3",3) );
 	time_t date3;
 	listeMesurebyDate.insert( std::pair<time_t,map<string,int>>(date3,listeMesurebyAttribut) );
 
 	listeMesurebyAttribut.insert( std::pair<string,int>("O3",50) );
-	listeMesurebyAttribut.insert( std::pair<string,int>("N3",50) );
+	listeMesurebyAttribut.insert( std::pair<string,int>("N3",4) );
 	time_t date4;
 	listeMesurebyDate.insert( std::pair<time_t,map<string,int>>(date4,listeMesurebyAttribut) );
 
 	listeMesurebyAttribut.insert( std::pair<string,int>("O3",50) );
-	listeMesurebyAttribut.insert( std::pair<string,int>("N3",50) );
+	listeMesurebyAttribut.insert( std::pair<string,int>("N3",5) );
 	time_t date5;
 	listeMesurebyDate.insert( std::pair<time_t,map<string,int>>(date5,listeMesurebyAttribut) );
 
 	listeMesurebyCapteur.insert( std::pair<string,map<time_t,map<string,int>>>("capteur1",listeMesurebyDate) );
-	Attribut att = Attribut("N3","g/m3","description",30);
+	Attribut att = Attribut("NO6","g/m3","description",30);
 	Warning mesWarnings = Warning(1,"N3");
 	cout<<mesWarnings.calculerDonneePrevisionelle("capteur1",listeMesurebyCapteur,att)<<endl;
 	return 0;
