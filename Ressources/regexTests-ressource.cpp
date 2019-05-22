195794043bac546e82a45cbed9fa1a1378e855a3
solenebutruille <47445746+solenebutruille@users.noreply.github.com>
2019-05-22 15:10:24 +0200

    Add files via upload

--- Ressources/regexTests-ressource.cpp
+++ Ressources/regexTests-ressource.cpp
@@ -11,7 +11,7 @@ int main ()
 {
     
     string line = "2017-01-01T00:01:20.6090000;Sensor0;O3;17.8902017543936";
-    regex capteurRegex("(.*);(.*);(.*);(.*);");
+    regex capteurRegex("(.*);(.*);(.*);(.*)");
     smatch matches;
     
     if (regex_search(line, matches, capteurRegex))
@@ -23,16 +23,21 @@ int main ()
     		string sensorId = matches[2].str();
     		string attributeId = matches[3].str();
     		string valueS = matches[4].str();
-    		int value = atoi(valueS.c_str());
-    		cout << value;
+    		float value = stof(valueS.c_str());
+    		cout << timestampS << endl; 
+    		cout << sensorId << endl; 
+    		cout << attributeId << endl; 
+    		cout << valueS << endl; 
+    		cout << value << endl; 
     		regex timeRegex("(.*-.*-.*)T(.*:.*:.*)\\..*"); /*devrait marcher mais pas sur, à tester sur un PC qui peut compiler avec des lines comme celles du csv*/
     		if (regex_search(timestampS, matchesTime, timeRegex))
     		{
     			if (matchesTime[0].str() == timestampS)
-    			{
+    			{	
+				cout << matchesTime[1].str() << endl << matchesTime[2].str() << endl;
     				struct tm tm;
     				istringstream ss(matchesTime[1].str() + "-" + matchesTime[2].str());
-    				ss >> std::get_time(&tm, "%F-%T"); // or just %T in this case
+    				ss >> std::get_time(&tm, "%F-%T");
     				time_t timestamp = mktime(&tm);
     			}
     		}
