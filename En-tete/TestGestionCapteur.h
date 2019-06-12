06e89f39a31744f4f8ff721e4033b291aff74407
BriceVachez <50611448+BriceVachez@users.noreply.github.com>
2019-06-13 00:29:12 +0200

    Add files via upload

---
+++ En-tete/TestGestionCapteur.h
@@ -0,0 +1,73 @@
+﻿#pragma once
+/*************************************************************************
+TestGestionCapteur -  description
+-------------------
+d�but                : 22/05/2019
+copyright            : (C) 2019 par CLEMENCEAU
+*************************************************************************/
+
+//---------- Interface de la classe <TestGestionCapteur> (fichier TestGestionCapteur.h) ------
+#if ! defined ( TESTGESTIONCAPTEURH )
+#define TESTGESTIONCAPTEUR_H
+
+//--------------------------------------------------- Interfaces utilis�es
+#include <string>
+using namespace std;
+//------------------------------------------------------------- Constantes
+
+//------------------------------------------------------------------ Types
+
+//------------------------------------------------------------------------
+// R�le de la classe <TestGestionCapteur>
+//
+//
+//------------------------------------------------------------------------
+
+class TestGestionCapteur
+{
+	//----------------------------------------------------------------- PUBLIC
+
+public:
+
+    TestGestionCapteur();
+
+    virtual ~TestGestionCapteur();
+	//----------------------------------------------------- M�thodes publiques
+
+	void testUnitaire(string filename);
+	// Mode d'emploi :
+	// teste toutes les méthodes de la classe GestionCapteur
+	// affiche les erreurs s'il y en a
+
+protected:
+	//----------------------------------------------------- M�thodes prot�g�es
+    void testFichier(string filename);
+
+    void testAjouter();
+
+    void testSuppressionID();
+
+    void testSuppressionLatLng();
+
+    void testRechercheID();
+
+    void testRechercheLatLng();
+private:
+	//------------------------------------------------------- M�thodes priv�es
+
+protected:
+	//----------------------------------------------------- Attributs prot�g�s
+
+private:
+	//------------------------------------------------------- Attributs priv�s
+
+	//---------------------------------------------------------- Classes amies
+
+	//-------------------------------------------------------- Classes priv�es
+
+	//----------------------------------------------------------- Types priv�s
+
+};
+
+
+#endif // TESTGESTIONCAPTEUR_H
---
+++ En-tete/TestGestionMesure.h
@@ -0,0 +1,68 @@
+﻿#pragma once
+/*************************************************************************
+TestGestionMesure -  description
+-------------------
+d�but                : 22/05/2019
+copyright            : (C) 2019 par CLEMENCEAU
+*************************************************************************/
+
+//---------- Interface de la classe <TestGestionMesure> (fichier TestGestionMesure.h) ------
+#if ! defined ( TESTGESTIONMESURE_H )
+#define TESTGESTIONMESURE_H
+
+//--------------------------------------------------- Interfaces utilis�es
+#include <iostream>
+#include <string>
+#include "../En-tete/GestionMesure.h"
+#include "../En-tete/Mesure.h"
+using namespace std;
+//------------------------------------------------------------- Constantes
+
+//------------------------------------------------------------------ Types
+
+//------------------------------------------------------------------------
+// R�le de la classe <TestGestionMesure>
+//
+//
+//------------------------------------------------------------------------
+
+class TestGestionMesure
+{
+	//----------------------------------------------------------------- PUBLIC
+
+public:
+
+	//----------------------------------------------------- M�thodes publiques
+
+	void testUnitaire(string filename, string filename2);
+	// Mode d'emploi :
+	// teste toutes les méthodes de la classe GestionMesure
+	// affiche les erreurs s'il y en a
+
+protected:
+	//----------------------------------------------------- M�thodes prot�g�es
+
+    void testFichierAtt(string filename);
+
+    void testFichierMes(string filename);
+
+    void testAjouterAtt();
+private:
+	//------------------------------------------------------- M�thodes priv�es
+
+protected:
+	//----------------------------------------------------- Attributs prot�g�s
+
+private:
+	//------------------------------------------------------- Attributs priv�s
+
+	//---------------------------------------------------------- Classes amies
+
+	//-------------------------------------------------------- Classes priv�es
+
+	//----------------------------------------------------------- Types priv�s
+
+};
+
+
+#endif // TESTGESTIONMESURE_H
